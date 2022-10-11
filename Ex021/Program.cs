72d430b640fa1e4d42fc9a3335abf6e86a7ec926
IvanK <mr.koldamasov@mail.ru>
2022-10-11 22:01:51 +0300

    Высчитывает расстояние между двумя заданными точками в 2d

---
+++ Ex021/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Clear();
+Console.WriteLine("Напиште коардинаты x1: ");
+Console.WriteLine("Напиште коардинаты y1: ");
+
+double x1 = Convert.ToInt32(Console.ReadLine());
+double y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Напиште коардинаты x2 ");
+Console.WriteLine("Напиште коардинаты y2: ");
+
+double x2 = Convert.ToInt32(Console.ReadLine());
+double y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Round(Math.Sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)), 2));
