889ce219350c5e5d609541f65a7f1c2a9b564842
IvanK <mr.koldamasov@mail.ru>
2022-10-11 21:32:20 +0300

    показывает диапазон возможных координат точек в заданной четверти

---
+++ Ex018/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 18: Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Clear();
+Console.WriteLine("Напиште четверть: ");
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x == 1)
+    Console.WriteLine("(x > 0 && y > 0)");
+else if (x == 2)
+    Console.WriteLine("(x < 0 && y > 0)");
+else if (x == 3)
+    Console.WriteLine("(x < 0 && y < 0)");
+else
+    Console.WriteLine("(x < 0 && y > 0)");
