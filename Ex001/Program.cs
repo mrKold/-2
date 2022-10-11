11881e3be93db4d75ef3b25e1284220ac20c01bc
IvanK <mr.koldamasov@mail.ru>
2022-10-11 21:17:41 +0300

    выдаёт номер четверти плоскости заданной точки коардинат, с проверкой на 0

---
+++ Ex001/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Напишите программу, которая принимает на вход координаты точки (X и Y), 
+причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта 
+точка*/
+
+Console.Clear();
+Console.WriteLine("Напиште коардинаты x: ");
+Console.WriteLine("Напиште коардинаты y: ");
+
+double x = Convert.ToInt32(Console.ReadLine());
+double y = Convert.ToInt32(Console.ReadLine());
+
+while (x == 0)
+{
+    Console.Write("Напиште коардинаты x: ");
+    x = Convert.ToDouble(Console.ReadLine());
+}
+while (y == 0)
+{
+    Console.Write("Напиште коардинаты y: ");
+    y = Convert.ToDouble(Console.ReadLine());
+}
+
+if (x > 0 && y > 0)
+    Console.WriteLine("I");
+else if (x < 0 && y > 0)
+    Console.WriteLine("II");
+else if (x < 0 && y < 0)
+    Console.WriteLine("III");
+else
+    Console.WriteLine("IV");
