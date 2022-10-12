76cddbb7b9a17746d20b230e35f7a292584d003b
IvanK <mr.koldamasov@mail.ru>
2022-10-12 16:15:56 +0300

    Проверка числа на палиндром

---
+++ HomeTask019/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число: ");
+string n = Console.ReadLine();
+
+
+while (n.Length <= 4 || n.Length >= 6)
+{
+    Console.WriteLine("Вы ошиблись! Введите ПЯТИЗНАЧНОЕ число!: ");
+    n = Console.ReadLine();
+}
+
+if (n[0] == n[4] && n[1] == n[3])
+{
+    Console.WriteLine("палиндром");
+}
+else
+{
+    Console.WriteLine("не палиндром");
+}
